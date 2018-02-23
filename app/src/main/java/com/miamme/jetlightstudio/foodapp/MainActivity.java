7ba8e03e09859c0e2da4d35b07583d46a763b105
Oussama BONNOR <oussamabonnor@gmail.com>
2018-02-23 19:35:26 +0100

    adding simple menu bar with "about" button

--- app/src/main/java/com/miamme/jetlightstudio/foodapp/MainActivity.java
+++ app/src/main/java/com/miamme/jetlightstudio/foodapp/MainActivity.java
@@ -2,11 +2,14 @@ package com.miamme.jetlightstudio.foodapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText textField;
@@ -41,5 +44,20 @@ public class MainActivity extends AppCompatActivity {
     public void printDB() {
         listView.setAdapter(helper.readFromDB(this));
         textField.setText("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.about) {
+            Toast.makeText(this, "Made by JetLight studio", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
